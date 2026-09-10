0e4b951cecd2c07940ed1811630858f9d3a65b7a
GlazerMann <kurt.glaesemann@pnnl.gov>
2026-09-10 07:51:12 -0500

    Add tests for secure_rnode_ports functionality

--- ood-portal-generator/spec/ood_portal_generator_view_spec.rb
+++ ood-portal-generator/spec/ood_portal_generator_view_spec.rb
@@ -22,4 +22,21 @@ describe OodPortalGenerator::View do
       expect(config_opts + example_config_opts - (config_opts & example_config_opts)).to be_empty
     end
   end
+
+  describe 'secure_rnode_ports' do
+    it 'does not set an allowlist environment value by default' do
+      expect(described_class.new.secure_rnode_ports_env).to be_nil
+    end
+
+    it 'normalizes a bounded explicit port allowlist' do
+      view = described_class.new(secure_rnode_ports: [8443, '443', 443])
+      expect(view.secure_rnode_ports_env).to eq('443,8443')
+    end
+
+    it 'rejects invalid ports and oversized lists' do
+      [443, [], [0], [65_536], ['443x'], (1..65).to_a].each do |ports|
+        expect { described_class.new(secure_rnode_ports: ports) }.to raise_error(ArgumentError)
+      end
+    end
+  end
 end
